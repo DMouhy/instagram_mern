4572106b6c62f4beaa848113b4b7e912f8ae1beb
DMouhy <50054600+DMouhy@users.noreply.github.com>
2020-10-02 17:33:03 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.df8d5dea3fcc586cbec4592d93009ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441fa9f2eb704a14157ad08ed0092d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76040e16de2ac5a2636b",
+    "url": "/static/css/main.2d3c2f35.chunk.css"
+  },
+  {
+    "revision": "41de22cf604ed7846a26",
+    "url": "/static/js/2.627155e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.627155e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76040e16de2ac5a2636b",
+    "url": "/static/js/main.07695501.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
